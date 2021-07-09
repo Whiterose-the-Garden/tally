2ba42c79d89bab1d4c456e9bab7dd423870d293d
sm-hwang <steven.m.hwang@gmail.com>
2021-07-08 20:33:35 -0600

    Initial Commit

---
+++ Tally/TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyApp.swift
+//  Tally
+//
+//  Created by Munsu Hwang on 2021-07-08.
+//
+
+import SwiftUI
+
+@main
+struct TallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
